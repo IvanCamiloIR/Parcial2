ba7f9a042b788fa7482fd68cb8adbe02b17f8596
IvanCamiloIR <165061472+IvanCamiloIR@users.noreply.github.com>
2024-08-21 16:06:29 -0500

    Update main_parcial.py

--- main_parcial.py
+++ main_parcial.py
@@ -3,7 +3,7 @@ def main ():
 
     print("Respuesta pregunta 1:", parcial2.repetidos ((1,2,4,3,2)))
     print("Respuesta pregunta 2:", parcial2.str_in_lista (("hola", "hada", "adios"), "hada"))
-    print(""Respuesta pregunta 3:",parcial2.diferencias_de_listas ([1,2,3],[1,2]))
+    print("Respuesta pregunta 3:",parcial2.diferencias_de_listas ([1,2,3],[1,2]))
     print("Respuesta pregunta 4:", parcial2.promedio_arreglo ([1,2,3,4,5]))
     print("Respuesta pregunta 5:", parcia2.mediana ((2, 5, 6, 5)))
 
